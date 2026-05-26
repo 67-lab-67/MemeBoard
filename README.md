015b7a5e372e9268535d63b3ed0fa14f2e3c8c96
kesavan17 <kesavannadar17@gmail.com>
2026-05-26 12:45:01 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,72 @@
+# memesounds
+
+A Python package for playing meme sounds.
+
+## Quick start
+
+```python
+import memesounds
+
+memesounds.fart()
+memesounds.play_sound("six_seven")
+memesounds.play_sound("67")
+memesounds.random_sound()
+THERE ARE OTHER 20 SOUNDS TRY IT OUT..
+
+## Command line
+
+From the project folder:
+
+```powershell
+python -m memesounds --list
+python -m memesounds fart
+python -m memesounds 67
+python -m memesounds --random
+```
+
+After installing the package, you can also use:
+
+```powershell
+memesounds --list
+memesounds fart
+```
+
+## Available sounds
+
+- `baby`
+- `bone_crack`
+- `fart`
+- `oppa`
+- `siren`
+- `six_seven`
+- `wow`
+
+```ABOUT THE CREATOR ```
+
+HI, I AM KESAVAN NADAR FROM INDIA, I KNOW YOU CAME HERE BECAUSE U WANT TO USE MEME SOUNDS IN YOUR PROJECTS TO MAKE IT FUN RIGHT
+ITS MY FIRST PACKAGE OF MY LIFE YEAH I MESSED UP A LITTLE BUT YEAH
+I HAVE MY OWN CHANNEL WHERE I POST EDITS IN YT - k17_editz
+I DID AI CODING FIRST WHEN I WAS 10 LIKE MY JARVIS ITS A WEBSITE AND A FAR MORE BETTER PERSONAL VOICE AND IMAGE GENERATING FRIEND THAN MY BROTHERS ONE WHICH HE CREATED AT 12
+https://k17-d4.netlify.app/
+https://mark-k17-d.netlify.app/
+https://mark-k17-c.netlify.app/
+https://k17-b.netlify.app/
+
+THE ABOVE LINKS ARE JUST THE VERSIONS OF MY JARVIS//...
+
+THIS IS AN A.M.I.S- AUTOMATED MOCK INTERVIEW SIMULATOR 
+ https://automated-mock-interview-simulator.netlify.app/
+
+CHILDREN FRIENDLY TUG OF WAR GAME https://bmw-m3-gtr90.netlify.app/
+
+I HAVE ALSO HAVE EXPERIENCE OF RASPBERRY PI 4, ARDUINO UNO, ESP32 LIKE MODULES PROJECTS
+
+~T~                 
+~H~
+~A~
+~N~
+~K~
+
+~Y~
+~O~
+~U~
--- sounds.py
+++ sounds.py
@@ -1,19 +1,56 @@
 from pathlib import Path
+import argparse
+import random
 import winsound
 
 SOUNDS_DIR = Path(__file__).parent / "assets"
+
+ALIASES = {
+    "oppa": "oopa",
+    "six_seven": "67",
+}
+
+
+def _sound_path(sound_name):
+    sound_key = ALIASES.get(sound_name, sound_name)
+    return SOUNDS_DIR / f"{sound_key}.wav"
+
+
+def list_sounds():
+    return sorted(sound.stem for sound in SOUNDS_DIR.glob("*.wav"))
+
+
+def play_sound(sound_name):
+    sound_file = _sound_path(sound_name)
+
+    if not sound_file.exists():
+        print(f"Sound '{sound_name}' not found at {sound_file.resolve()}!")
+        return False
+
+    print(f"Now Playing: {sound_file.stem}")
+    winsound.PlaySound(str(sound_file), winsound.SND_FILENAME)
+    return True
+
+
+def random_sound():
+    sounds = list_sounds()
+    if not sounds:
+        print(f"No sounds found in {SOUNDS_DIR.resolve()}!")
+        return False
+
+    return play_sound(random.choice(sounds))
+
 
 def play(sound_name, meme_label):
     sound_file = SOUNDS_DIR / f"{sound_name}.wav"
 
     if not sound_file.exists():
         print(f"Sound '{sound_name}' not found at {sound_file.resolve()}!")
-        return
+        return False
 
     print(f"Now Playing: {meme_label}")
-    # SND_FILENAME: Tells it to look for a file
-    # SND_ASYNC: Plays the sound in the background so the script doesn't freeze
-    winsound.PlaySound(str(sound_file), winsound.SND_FILENAME | winsound.SND_ASYNC)
+    winsound.PlaySound(str(sound_file), winsound.SND_FILENAME)
+    return True
 
 # --- Meme Functions ---
 
@@ -24,7 +61,7 @@ def bone_crack():
     play("bone_crack", "Bone_crack Meme")
 
 def oppa():
-    play("oppa", "Oopa Neymar Meme")
+    play("oopa", "Oopa Neymar Meme")
 
 def six_seven():
     play("67", "67 Meme")
@@ -33,7 +70,62 @@ def siren():
     play("siren", "-999 Social Credit Siren Meme") 
 
 def wow():
-    play("WOW", "WoW Meme")    
+    play("wow", "woooow Meme")    
 
 def baby():
-    play("baby", "Baby_cry Meme")
+    play("baby", "Baby_laugh Meme") 
+
+def ack ():
+    play ("ack", "ack Meme")    
+
+def cat_laugh ():
+    play ("cat_laugh", "cat_laugh Meme")
+
+def get_out ():
+    play ("get_out", "get_out Meme")
+
+def emo_damage ():
+    play ("emo_damage", "emotional damage Meme")
+
+def hell_nahh ():
+    play ("hell_nahh", "Hell_nahh Meme")   
+
+def mario_jump ():
+    play ("mario_jump", "mario_jump")
+
+def omg():
+    play ("omg", "ohh my god")                     
+
+def sad_meow ():
+    play ("sad_meow", "so sad")
+
+def so_cute ():
+    play ("so_cute", "i show speed")
+
+def womp_womp ():
+    play ("womp_womp", "so sad again")
+
+def yay ():
+    play ("yay", "be happy")
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(prog="memesounds")
+    parser.add_argument("sound", nargs="?", help="Sound name to play")
+    parser.add_argument("--list", action="store_true", help="List available sounds")
+    parser.add_argument("--random", action="store_true", help="Play a random sound")
+    args = parser.parse_args(argv)
+
+    if args.list:
+        for sound in list_sounds():
+            print(sound)
+        return 0
+
+    if args.random:
+        return 0 if random_sound() else 1
+
+    if args.sound:
+        return 0 if play_sound(args.sound) else 1
+
+    parser.print_help()
+    return 0
